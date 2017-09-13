9daf7ccb77011d9bc0facd352188fe852fbaecdf
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2017-09-13 13:52:04 +0200

    Refactor into page object

--- basics.py
+++ basics.py
@@ -20,37 +20,44 @@ while links_limit < 1:
             print("Couldn't parse that, try again")
 
 
+class RedditPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def get_links(self):
+        return self.driver.find_elements_by_css_selector('a.title')
+
+    def navigate_to_next_page(self):
+        next_button = self.driver.find_element_by_css_selector('span.next-button a')
+        next_button.click()
+
+
 file_dir = os.path.dirname(os.path.realpath(__file__))
 driver_path = os.path.join(file_dir, 'geckodriver.exe')
-driver = webdriver.Firefox(executable_path=driver_path)
-driver.set_page_load_timeout(5)
+web_driver = webdriver.Firefox(executable_path=driver_path)
+web_driver.set_page_load_timeout(10)
 
-driver.get('https://reddit.com/r/' + subreddit)
+web_driver.get('https://reddit.com/r/' + subreddit)
+
+page = RedditPage(web_driver)
 
 links_found = 0
 links = deque()
 
-
-def get_links(driver: webdriver):
-    return driver.find_elements_by_css_selector('a.title')
-
-
-links.extend(get_links(driver))
+links.extend(page.get_links())
 links_considered = 0
 
 while links_considered < links_limit:
     if len(links) == 0:
-        next_button = driver.find_element_by_css_selector('span.next-button a')
-        next_button.click()
-        links.extend(get_links(driver))
+        page.navigate_to_next_page()
+        links.extend(page.get_links())
 
     link = links.pop()
     if keyword in link.text.lower():
         links_found += 1
     links_considered += 1
 
-driver.close()
+web_driver.close()
 
 print('Found {} matches in {} links'.format(links_found, links_considered))
 print('Hit rate: {:.1f}%'.format(100 * links_found / links_considered))
-
